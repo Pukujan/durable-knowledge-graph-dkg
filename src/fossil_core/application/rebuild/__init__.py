366172bf01f21b46397c96630b9597ca79bd6d40
Pujan <137629468+Pukujan@users.noreply.github.com>
2026-08-17 13:35:18 -0400

    [ARCH-P4] Add application rebuild package

---
+++ src/fossil_core/application/rebuild/__init__.py
@@ -0,0 +1,5 @@
+"""Rebuildable application projections derived from durable FOSSIL state."""
+
+from .pack_corpus import retrieval_documents_from_pack_fixtures
+
+__all__ = ["retrieval_documents_from_pack_fixtures"]
